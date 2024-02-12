564e9d56534a2c1a7ac364847c719c52d6a846cb
70CentsApple <Chris48@qq.com>
2024-02-12 18:58:34 +0800

    chore: remove tabs

--- src/main/java/net/apple70cents/chattools/features/chatkeybindings/Repeat.java
+++ src/main/java/net/apple70cents/chattools/features/chatkeybindings/Repeat.java
@@ -24,17 +24,17 @@ public class Repeat {
                 MinecraftClient mc = MinecraftClient.getInstance();
                 String mcVersion = FabricLoader.getInstance().getModContainer("minecraft").get().getMetadata()
                                                .getVersion().getFriendlyString();
-                if ("1.20".equals(mcVersion) || "1.20.1".equals(mcVersion)){
+                if ("1.20".equals(mcVersion) || "1.20.1".equals(mcVersion)) {
                     MessageUtils.sendToActionbar(TextUtils.trans("texts.repeat.failure.unsupported"));
                     return;
                 }
-                    // Note:
-                    // Between versions 1.20.1 ~ 1.20.2, the return value type of `getMessageHistory()` had
-                    // changed from `List` to `net.minecraft.util.collection.ArrayListDeque` (a.k.a `net/minecraft/class_8623` ),
-                    // so if this code is compiled on 1.20.2+, there will be a mapping problem here on both 1.20 and 1.20.1,
-                    // which can be solved by recompiling on either of those versions individually.
-                    // I don't think it's a big problem, not gonna fix this ^v^
-                    List<String> history = mc.inGameHud.getChatHud().getMessageHistory();
+                // Note:
+                // Between versions 1.20.1 ~ 1.20.2, the return value type of `getMessageHistory()` had
+                // changed from `List` to `net.minecraft.util.collection.ArrayListDeque` (a.k.a `net/minecraft/class_8623` ),
+                // so if this code is compiled on 1.20.2+, there will be a mapping problem here on both 1.20 and 1.20.1,
+                // which can be solved by recompiling on either of those versions individually.
+                // I don't think it's a big problem, not gonna fix this ^v^
+                List<String> history = mc.inGameHud.getChatHud().getMessageHistory();
                 if (history.isEmpty()) {
                     MessageUtils.sendToActionbar(TextUtils.trans("texts.repeat.failure.empty"));
                 } else {
